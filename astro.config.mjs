2bd3a3c903424df98e269976d2f94f8080277a76
Paulneo <jpaulneo2k@gmail.com>
2024-03-14 23:12:55 -0500

    ..

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
@@ -7,5 +6,8 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), react()],
+  integrations: [sitemap(), tailwind(), react()],
+  image: {
+    service: passthroughImageService(),
+  },
 });
