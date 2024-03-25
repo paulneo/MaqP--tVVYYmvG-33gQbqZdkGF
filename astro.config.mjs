4a4246f924e1aa44d51233db2dfce188ec40065b
Paulneo <jpaulneo2k@gmail.com>
2024-03-25 17:44:45 -0500

    partydown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,23 @@ import { defineConfig, passthroughImageService } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-
 import netlify from '@astrojs/netlify';
+
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.maquinariaspesadas.org',
-  integrations: [sitemap(), tailwind(), react()],
+  integrations: [
+    sitemap(),
+    tailwind(),
+    react(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
   image: {
     service: passthroughImageService(),
   },
