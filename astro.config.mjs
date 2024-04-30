4ab0786c9bae7d6d7eba4f0c2600c129482a511e
paulneo <jpaulneo2k@gmail.com>
2024-04-30 16:52:28 -0500

    robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,28 @@
-import { defineConfig, passthroughImageService } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-import netlify from '@astrojs/netlify';
+import { defineConfig, passthroughImageService } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
 
-import partytown from '@astrojs/partytown';
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.maquinariaspesadas.org',
+  site: "https://mundoingenieria.org",
   integrations: [
     sitemap(),
     tailwind(),
     react(),
     partytown({
       config: {
-        forward: ['dataLayer.push'],
+        forward: ["dataLayer.push"],
       },
     }),
   ],
   image: {
     service: passthroughImageService(),
   },
-  output: 'server',
+  output: "server",
   adapter: netlify({
     cacheOnDemandPages: true,
   }),
